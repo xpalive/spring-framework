98060d75e6216db2e8af969aca5d2e4323954f7e
xiongping <xpalive@gmail.com>
2021-11-14 10:48:05 +0800

    强制回滚

--- xp-alive/src/main/java/com/xiongping/service/UserService2.java
+++ xp-alive/src/main/java/com/xiongping/service/UserService2.java
@@ -15,7 +15,7 @@ public class UserService2 implements UserServiceFacade {
 
 	@Transactional
 	public void test(){
-		jdbcTemplate.execute("select * from ums_user");
+		jdbcTemplate.execute("insert into ums_user values (2,'xiongping',34)");
 		System.out.println("test");
 //		TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();// 强制回滚
 	}
