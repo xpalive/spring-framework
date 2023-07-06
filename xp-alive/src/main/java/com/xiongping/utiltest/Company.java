8ea34886a95d5488ee6e5d545c1c22305d770561
xiongping <xpalive@gmail.com>
2023-07-06 21:27:02 +0800

    更新注释

--- xp-alive/src/main/java/com/xiongping/utiltest/Company.java
+++ xp-alive/src/main/java/com/xiongping/utiltest/Company.java
@@ -1,13 +1,26 @@
 package com.xiongping.utiltest;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Component;
 
-@Configuration
-public class Company {
+import javax.annotation.PostConstruct;
 
-//	@Import(Member.class)
+@Configuration
+public class Company implements InitializingBean {
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("first");
+	}
+
+
+	public void postConstruct(){
+		System.out.println("second");
+	}
+
+	//	@Import(Member.class)
 	public class Department{
 
 
