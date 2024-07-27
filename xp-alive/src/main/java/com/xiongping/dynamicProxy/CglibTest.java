01a9e2ed03585f14f0f35fc45102e249a345fde5
xiongping <xpalive@gmail.com>
2024-07-27 14:49:15 +0800

    动态代理

--- xp-alive/src/main/java/com/xiongping/dynamicProxy/CglibTest.java
+++ xp-alive/src/main/java/com/xiongping/dynamicProxy/CglibTest.java
@@ -1,38 +1,50 @@
 package com.xiongping.dynamicProxy;
 
-import org.springframework.cglib.proxy.*;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.CallbackFilter;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.NoOp;
 
 import java.lang.reflect.Method;
 
 public class CglibTest {
-	public static void main(String[] args) {
-		UserServiceImpl target = new UserServiceImpl();
-
-		Enhancer enhancer = new Enhancer();
-		enhancer.setSuperclass(UserServiceImpl.class);
-		enhancer.setCallbacks(new Callback[]{(MethodInterceptor) (o, method, objects, methodProxy) -> {
-			System.out.println("before");
-//				final Object result = methodProxy.invoke(target, objects);
-//				final Object result = method.invoke(target, objects);
-//				final Object result = method.invoke(o, objects); //报错，执行代理类的当前方法，会栈溢出
-			final Object result = methodProxy.invokeSuper(o, objects);
-			System.out.println("after");
-			return result;
-		}, NoOp.INSTANCE
-		});
-
-		enhancer.setCallbackFilter(new CallbackFilter() {
-			@Override
-			public int accept(Method method) {
-				if (method.getName().equals("testVoid")) {
-					return 0;
-				}
-				return 1;
-			}
-		});
-
-		final UserServiceImpl userService = (UserServiceImpl) enhancer.create();
-//		System.out.println(userService.test());
-		userService.test();
-	}
+    
+    public static void main(String[] args) {
+        UserServiceImpl target = new UserServiceImpl();
+        
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(UserServiceImpl.class);
+        // o 是代理对象
+        // Method 是被代理对象的方法
+        // objects是传入的参数
+        // methodProxy是代理方法
+        enhancer.setCallbacks(new Callback[] {(MethodInterceptor) (o, method, objects, methodProxy) -> {
+            System.out.println("before");
+            //通过原始对象 请求代理方法
+                        final Object result = methodProxy.invoke(target, objects);
+            //通过原始对象 请求原始对象的方法
+//                        final Object result = method.invoke(target, objects);
+            // 通过代理对象请求 原始对象的方法
+//            final Object result = method.invoke(o, objects); //报错，执行代理类的当前方法，会栈溢出
+            // 通过 代理对象请求父类的方法
+//            final Object result = methodProxy.invokeSuper(o, objects);
+            System.out.println("after");
+            return result;
+        }, NoOp.INSTANCE});
+        
+        enhancer.setCallbackFilter(new CallbackFilter() {
+            @Override
+            public int accept(Method method) {
+                if (method.getName().equals("testVoid")) {
+                    return 0;
+                }
+                return 1;
+            }
+        });
+        
+        final UserServiceImpl userService = (UserServiceImpl) enhancer.create();
+        //		System.out.println(userService.test());
+        userService.testVoid();
+    }
 }
--- xp-alive/src/main/java/com/xiongping/dynamicProxy/JdkTest.java
+++ xp-alive/src/main/java/com/xiongping/dynamicProxy/JdkTest.java
@@ -3,13 +3,20 @@ package com.xiongping.dynamicProxy;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+/**
+ * jdk的动态代理只能通过接口来实现
+ * 通过传入被代理的类的实例来调用原始逻辑
+ * 被代理的类需要实现接口才可以通过反射方式调用对应的方法，否则就需要主动调用
+ */
 public class JdkTest {
 	public static void main(String[] args) {
-		final UserService userService = new UserServiceImpl();
+		
+		final UserServiceImpl userService = new UserServiceImpl();
 
 		final UserService proxyInstance = (UserService) Proxy.newProxyInstance(JdkTest.class.getClassLoader(), new Class[]{UserService.class},
 				(Object proxy, Method method, Object[] args1) -> {
 					System.out.println("before");
+//					userService.testVoid();
 					return method.invoke(userService, args1);
 				});
 		proxyInstance.testVoid();
