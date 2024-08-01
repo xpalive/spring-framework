a49a6a0fe947a1dcaa434530958010e032c3dcef
xiongping <xpalive@gmail.com>
2024-08-01 23:58:25 +0800

    事务

---
+++ xp-alive/src/main/java/com/xiongping/transacation/ConfigTransaction.java
@@ -0,0 +1,13 @@
+package com.xiongping.transacation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author xiongping
+ * @date 2024-08-01
+ */
+@Configuration
+@EnableTransactionManagement
+public class ConfigTransaction {
+}
